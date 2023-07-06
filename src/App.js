d36a64f42c863d63ac31c6efaa4022ce8ce19926
rohith.r1 <rohith.r@comviva.com>
2023-07-06 22:06:08 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { HomeOutlined, UserOutlined, TrophyOutlined, ContactsOutlined } from '@ant-design/icons';
+//import 'antd/dist/antd.css';
 import './App.css';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<HomeOutlined />}>
+            Home
+          </Menu.Item>
+          <Menu.Item key="2" icon={<UserOutlined />}>
+            About
+          </Menu.Item>
+          <Menu.Item key="3" icon={<TrophyOutlined />}>
+            Achievements
+          </Menu.Item>
+          <Menu.Item key="4" icon={<ContactsOutlined />}>
+            Contact
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content"> Trying to Build my Profile Here</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}> Rohith R &copy; {new Date().getFullYear()}</Footer>
+    </Layout>
   );
 }
 

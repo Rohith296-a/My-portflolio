ed3a18a4f1919e138df29d7c73a5cc57a8a85fc3
rohith.r1 <rohith.r@comviva.com>
2023-07-06 22:18:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Card } from 'antd';
 import { HomeOutlined, UserOutlined, TrophyOutlined, ContactsOutlined } from '@ant-design/icons';
 //import 'antd/dist/antd.css';
 import './App.css';
+import 'animate.css';
 
 const { Header, Content, Footer } = Layout;
 
@@ -26,10 +27,45 @@ function App() {
           </Menu.Item>
         </Menu>
       </Header>
-      <Content style={{ padding: '0 50px' }}>
-        <div className="site-layout-content"> Trying to Build my Profile Here</div>
+      <Content className="content">
+        <div className="site-layout-content">
+          <h1 className="animate__animated animate__fadeInDown">Welcome to My Portfolio</h1>
+          <Card
+            title="About Me"
+            bordered={false}
+            style={{ width: 300 }}
+            className="card animate__animated animate__fadeInUp"
+          >
+            <p>
+              I am a passionate web developer with expertise in React.js,
+              JavaScript, and HTML/CSS.
+            </p>
+          </Card>
+          <Card
+            title="Achievements"
+            bordered={false}
+            style={{ width: 300 }}
+            className="card animate__animated animate__fadeInUp"
+          >
+            <ul>
+              <li>Completed a full-stack web development course</li>
+              <li>Published two mobile apps on App Store</li>
+            </ul>
+          </Card>
+          <Card
+            title="Contact Details"
+            bordered={false}
+            style={{ width: 300 }}
+            className="card animate__animated animate__fadeInUp"
+          >
+            <p>Email: example@example.com</p>
+            <p>Phone: 123-456-7890</p>
+          </Card>
+        </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}> Rohith R &copy; {new Date().getFullYear()}</Footer>
+      <Footer style={{ textAlign: 'center' }}>
+        Your Name &copy; {new Date().getFullYear()}
+      </Footer>
     </Layout>
   );
 }
